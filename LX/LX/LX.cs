ee622c0694cf51a99ac45cae9ff7886dd2f9088e
Lexxes <anfrbo@gmail.com>
2016-03-12 02:33:56 +0100

    removed a old code spam error

--- LX/LX/LX.cs
+++ LX/LX/LX.cs
@@ -13,7 +13,6 @@ namespace LX
 
 		private static void OnLoadingComplete(EventArgs args)
 		{
-			LX_Evade.SpellDedector.Initiate();
 			LX_Activator.Activator.Initiate();
 			//LX_Evade.Evade.Initiate();
 		}
